7a62af30da051cff1fb77d8568a32091672952b3
BuildX Dev Agent <buildx-agent@buildx.dev>
2026-04-14 12:10:28 -0400

    feat(buildx-agent): WIP: parcial

---
+++ frontend/src/components/users/UserForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const UserForm: React.FC = () => {
+  return (
+    <div>
+      <h2>User Form</h2>
+      {/* Form content will go here */}
+    </div>
+  );
+};
